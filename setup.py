3d89ded03dd7b96af68d2e2002cd0482a3ab5c8c
Jonny Vaughan <jonny@10degrees.uk>
2018-08-09 11:43:04 +0100

    change reqs

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from setuptools import setup, find_packages
 from os import environ as env
 import subprocess
 
-from pip.req import parse_requirements
+# from pip.req import parse_requirements
 
-requirements = [str(req.req) for req in parse_requirements('requirements.txt', session=False)]
-requirements_plugins = [str(req.req) for req in parse_requirements('requirements-plugins.txt', session=False)]
+# requirements = [str(req.req) for req in parse_requirements('requirements.txt', session=False)]
+# requirements_plugins = [str(req.req) for req in parse_requirements('requirements-plugins.txt', session=False)]
 
 try:
     VERSION = subprocess.check_output(['git', 'describe', '--tags']).strip()
@@ -24,7 +24,6 @@ setup(
     author_email='info@arachnys.com',
     url='http://cabotapp.com',
     license='MIT',
-    install_requires=requirements + requirements_plugins,
     packages=find_packages(),
     include_package_data=True,
     entry_points={
